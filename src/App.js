ac196b3be98072690b1df8606d1fc27e7b27a7d4
Frank Mathy <frankmathy@gmail.com>
2020-08-23 23:37:07 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello Frank</p>
+      <h1>Planning Poker</h1>
+      <Button>Start</Button>
     </div>
   );
 }
